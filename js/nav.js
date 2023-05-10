33ee79a63cd8f689e2af43d1f83f9b099ef54876
jb-2015 <juan_matias_becerra@hotmail.com>
2023-05-10 15:50:49 -0300

    UPDATE nav

---
+++ js/nav.js
@@ -0,0 +1,20 @@
+const menuBar= document.querySelector('.opcionesMenu');
+const enlaces= menuBar.querySelectorAll('a')
+
+
+enlaces.forEach(e=>{
+	e.addEventListener('mouseover', ()=>{
+		console.log("Entro en "+ e.textContent)
+		e.style.backgroundPosition = '0 0'
+		e.style.color= 'white'
+		e.style.background= 'black';
+
+	})
+
+	e.addEventListener('mouseout',()=>{
+		console.log("Salio de "+e.textContent)
+		e.style.backgroundPosition = '150% 0'
+		e.style.color= 'black'
+		e.style.removeProperty('background')
+	})
+})
